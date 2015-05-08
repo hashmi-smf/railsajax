3c89475293e939538196e3c13a07a03fab4b12fd
Hashmi <hashmi@cosmicvent.com>
2015-05-08 17:29:09 +0530

    added bootswatch theme and applied to the layout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,12 @@
 class ProductsController < ApplicationController
+  before_action :all_product, only:[:index, :create, :update, :destroy, :edit, :update] 
+  before_action :set_product, only:[:edit, :update, :destroy, :show]
   respond_to :html, :js
   
   def index
-    @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -14,18 +14,14 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @products = Product.all
     @product = Product.create(product_params)
   end
 
   def edit
-    @product = Product.find(params[:id])
+    
   end
 
   def update
-    @products = Product.all
-    @product = Product.find(params[:id])
-    
     @product.update_attributes(product_params)
   end
 
@@ -34,13 +30,18 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @products = Product.all
-    @product = Product.find(params[:id])
     @product.destroy
   end
 
 private
   def product_params
     params.require(:product).permit(:name, :price)
+  end
+
+  def all_product
+    @products = Product.all
+  end
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
